b98a0d5e9c9957284be93209b61c090252a190bb
jackc3 <yaobingxun93@gmail.com>
2025-05-28 10:18:03 +0800

    use app router auth()

--- apps/nextjs/src/trpc/server.ts
+++ apps/nextjs/src/trpc/server.ts
@@ -1,30 +1,29 @@
 import "server-only";
 
-import {cookies, headers} from "next/headers";
-import {createTRPCProxyClient, loggerLink, TRPCClientError} from "@trpc/client";
-import {experimental_createTRPCNextAppDirServer} from "@trpc/next/app-dir/server";
+import { cookies } from "next/headers";
+import { createTRPCProxyClient, loggerLink, TRPCClientError } from "@trpc/client";
 
-import {AppRouter} from "@saasfly/api";
+import { AppRouter } from "@saasfly/api";
 
-import {endingLink, transformer} from "./shared";
-import {observable} from "@trpc/server/observable";
+import { transformer } from "./shared";
+import { observable } from "@trpc/server/observable";
 import { callProcedure } from "@trpc/server";
-import {TRPCErrorResponse} from "@trpc/server/rpc";
+import { TRPCErrorResponse } from "@trpc/server/rpc";
 import { cache } from "react";
-import {NextRequest} from "next/server";
 import { appRouter } from "../../../../packages/api/src/root";
-import { getAuth } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 
-type AuthObject = ReturnType<typeof getAuth>;
+type AuthObject = Awaited<ReturnType<typeof auth>>;
 
 export const createTRPCContext = async (opts: {
-    headers: Headers;
-    auth: AuthObject;
+  headers: Headers;
+  auth: AuthObject;
+// eslint-disable-next-line @typescript-eslint/require-await
 }) => {
-    return {
-        userId: opts.auth.userId,
-        ...opts,
-    };
+  return {
+    userId: opts.auth.userId,
+    ...opts,
+  };
 };
 
 
@@ -32,51 +31,49 @@ export const createTRPCContext = async (opts: {
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
  */
-const createContext = cache(() => {
-    return createTRPCContext({
-        headers: new Headers({
-            cookie: cookies().toString(),
-            "x-trpc-source": "rsc",
-        }),
-        auth: getAuth(
-            new NextRequest("https://next-template-git-feature-clerk-saasfly.vercel.app/", {headers: headers()}),
-        ),
-    });
+const createContext = cache(async () => {
+  return createTRPCContext({
+    headers: new Headers({
+      cookie: cookies().toString(),
+      "x-trpc-source": "rsc",
+    }),
+    auth: await auth(),
+  });
 });
 
 export const trpc = createTRPCProxyClient<AppRouter>({
-    transformer,
-    links: [
-        loggerLink({
-            enabled: (op) =>
-                process.env.NODE_ENV === "development" ||
-                (op.direction === "down" && op.result instanceof Error),
+  transformer,
+  links: [
+    loggerLink({
+      enabled: (op) =>
+        process.env.NODE_ENV === "development" ||
+        (op.direction === "down" && op.result instanceof Error),
+    }),
+    /**
+     * Custom RSC link that lets us invoke procedures without using http requests. Since Server
+     * Components always run on the server, we can just call the procedure as a function.
+     */
+    () =>
+      ({op}) =>
+        observable((observer) => {
+          createContext()
+            .then((ctx) => {
+              return callProcedure({
+                procedures: appRouter._def.procedures,
+                path: op.path,
+                rawInput: op.input,
+                ctx,
+                type: op.type,
+              });
+            })
+            .then((data) => {
+              observer.next({result: {data}});
+              observer.complete();
+            })
+            .catch((cause: TRPCErrorResponse) => {
+              observer.error(TRPCClientError.from(cause));
+            });
         }),
-        /**
-         * Custom RSC link that lets us invoke procedures without using http requests. Since Server
-         * Components always run on the server, we can just call the procedure as a function.
-         */
-        () =>
-            ({op}) =>
-                observable((observer) => {
-                    createContext()
-                        .then((ctx) => {
-                            return callProcedure({
-                                procedures: appRouter._def.procedures,
-                                path: op.path,
-                                rawInput: op.input,
-                                ctx,
-                                type: op.type,
-                            });
-                        })
-                        .then((data) => {
-                            observer.next({result: {data}});
-                            observer.complete();
-                        })
-                        .catch((cause: TRPCErrorResponse) => {
-                            observer.error(TRPCClientError.from(cause));
-                        });
-                }),
-    ],
+  ],
 });
 export {type RouterInputs, type RouterOutputs} from "@saasfly/api";
